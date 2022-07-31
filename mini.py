4105e71345638a91f430a5c09d40f6a3bdb1d03a
een <107556837+Sabrinaizureen@users.noreply.github.com>
2022-07-31 12:40:05 +0800

    Update mini.py

--- mini.py
+++ mini.py
@@ -11,3 +11,29 @@ window = Tk()
 window['bg']='black'
 window.geometry("1350x700")
 window.title("Bus Online Booking")
+
+#----------------------WINDOW AND FRAME--------------------------
+topframe = Frame (window, width = 1350, height=100, bd=6, relief='raise', bg = 'dark goldenrod')
+topframe.pack(side= TOP)
+
+bottomframe = Frame (window,width = 1350, height=600,bd=6, relief='raise', bg = 'black')
+bottomframe.pack(side= BOTTOM)
+
+infoframe = Frame(bottomframe,width = 1350, height=250,bd=2, relief='raise', bg = 'dark goldenrod')
+infoframe.pack(side= TOP)
+ticketframe = Frame(bottomframe,width = 1350, height=200,bd=6, relief='raise', bg = 'dark goldenrod')
+ticketframe.pack(side= BOTTOM)
+btnframe = Frame(bottomframe,width = 1350, height=70,bd=6, relief='raise', bg = 'dark goldenrod')
+btnframe.pack(side= BOTTOM)
+
+leftinfo = Frame (infoframe,width = 450, height=250,bd=6, relief='raise', bg = 'black')
+leftinfo.pack(side= LEFT)
+rightinfo = Frame (infoframe,width = 300, height=250,bd=6, relief='raise', bg = 'black')
+rightinfo.pack(side= RIGHT)
+centerinfo = Frame (infoframe,width = 550, height=250,bd=6, relief='raise',bg = 'black')
+centerinfo.pack(side= RIGHT)
+
+dspframe = Frame (ticketframe,width = 300, height=200,bd=6, relief='raise', bg = 'dark goldenrod')
+dspframe.pack(side= LEFT)
+ticketinfo = Frame (ticketframe,width = 1050, height=200,bd=6, relief='raise', bg = 'dark goldenrod')
+ticketinfo.pack(side= RIGHT)
