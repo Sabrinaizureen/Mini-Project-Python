a25c1c829eb78b0f1dcece6df23a50b7799bd80b
shahrul88 <s211501514@studentmail.unimap.edu.my>
2022-07-31 12:37:21 +0800

    Update mini.py

--- mini.py
+++ mini.py
@@ -1,1 +1,8 @@
-from tkinter import* 
+from msilib.schema import Font
+from tkinter import*
+from tkinter import Tk, StringVar, ttk
+from tkinter import messagebox
+from turtle import right
+from tkcalendar import Calendar
+import sqlite3
+from PIL import Image, ImageTk
